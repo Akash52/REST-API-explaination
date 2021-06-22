823355fa7903f101b772bec2655ddc35655c7a73
Akash52 <ac8572611@gmail.com>
2021-06-22 17:02:46 +0530

    All user routes move to UserController

--- routes/users.js
+++ routes/users.js
@@ -1,52 +1,34 @@
 import express from 'express'
-import { v4 as uuid4 } from 'uuid'
+
+import {
+  getUser,
+  getUsers,
+  updateUser,
+  deleteUser,
+  createUser,
+} from '../controller/userController.js'
 
 const router = express.Router()
 
 let users = []
 //Get our all users
 
-router.get('/', (req, res) => {
-  res.send(users)
-  console.log(users)
-})
+router.get('/', getUsers)
 
 //Create our user
-//POST
 
-router.post('/', (req, res) => {
-  //console.log(req.body)
-  const user = req.body
-  users.push({ ...user, id: uuid4() })
-  console.log(user.lastName)
-  res.send('POST ROUTE REACHED !')
-})
+router.post('/', createUser)
 
 //Get Single USER
 
-router.get('/:id', (req, res) => {
-  const { id } = req.params
-  const foundUser = users.find((user) => user.id === id)
-  res.send(foundUser)
-})
+router.get('/:id', getUser)
 
 //Delete a single user
 
-router.delete('/:id', (req, res) => {
-  users = users.filter((user) => user.id !== req.params.id)
-
-  res.send('User deleted succesfully')
-})
+router.delete('/:id', deleteUser)
 
 //Update a user
 
-router.patch('/:id', (req, res) => {
-  const user = users.find((user) => user.id === req.params.id)
-
-  user.username = req.body.name
-  user.std = req.body.std
-
-  res.send('User updated succesfully')
-})
+router.patch('/:id', updateUser)
 
 export default router
